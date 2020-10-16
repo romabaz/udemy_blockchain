91c9d845f43b20d858118e48b294d00c3c8338a4
Roman Loyko <roman_loyko_tp@bmc.com>
2020-10-16 13:05:12 +0300

    Calculating the balance and mining transactions

--- app/miner.js
+++ app/miner.js
@@ -1,18 +1,24 @@
+const Wallet = require('../wallet');
+const Transaction = require('../wallet/transaction');
+
 class Miner {
   constructor(blockchain, transactionPool, wallet, p2pServer) {
     this.blockchain = blockchain;
     this.transactionPool = transactionPool;
     this.wallet = wallet;
     this.p2pServer = p2pServer;
+    console.log(this.wallet);
   }
 
   mine() {
     const validTransactions = this.transactionPool.validTransactions();
-    // include a reward for the miner
-    // create a block consisting of a valid transations
-    // sync the chains in the peer-2-peer server
-    // clear the transaction pool
-    // broadcast to every miner to clear their transaction pools
+    validTransactions.push(Transaction.rewardTransaction(this.wallet, Wallet.blockchainWallet()));
+    const block = this.blockchain.addBlock(validTransactions);
+    this.p2pServer.syncChains();
+    this.transactionPool.clear();
+    this.p2pServer.broadcastClearTransactions();
+
+    return block;
   }
 }
 
